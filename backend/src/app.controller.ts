d405a54fc8fc0ce6b050f003c86158c322690a83
Choi JinYoung <chio6632@gmail.com>
2024-12-24 08:45:10 +0900

    fix: app.controller 파일 초기화

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@ApiTags('초안')
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
