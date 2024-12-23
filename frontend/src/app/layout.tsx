e97c5653cd9f499b821977514d4e344d3e127460
서성진 <tjtjdwls0614@gmail.com>
2024-12-23 18:39:01 +0900

    feat: 혜더 컴포넌트 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,6 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import Header from './components/Layout/Header';
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -24,7 +14,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+            <body>
+                <Header />
+                {children}
+            </body>
         </html>
     );
 }
