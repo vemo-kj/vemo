22031ca528d1916cae072d5a1eda495432ff6506
SoohoPark <clapp301@gmail.com>
2024-12-26 12:59:32 +0900

    feat: Validation 코드 작성

--- backend/src/memos/dto/create-memos.dto.ts
+++ backend/src/memos/dto/create-memos.dto.ts
@@ -1,6 +1,23 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateMemosDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(50)
     title: string;
-    description?: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
+    description: string;
+
+    @IsNotEmpty()
+    @IsNumber()
     userId: number;
-    videoId: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    videoId: number;
 }
--- backend/src/memos/dto/update-memos.dto.ts
+++ backend/src/memos/dto/update-memos.dto.ts
@@ -1,4 +1,15 @@
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class UpdateMemosDto {
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(50)
     title?: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
     description?: string;
 }
