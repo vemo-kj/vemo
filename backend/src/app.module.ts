f1bd270c94939c695d291c59474ebcf8e3566886
Choi JinYoung <chio6632@gmail.com>
2024-12-26 15:19:05 +0900

    feat: db 연결 및 configModule  global로 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => typeOrmConfig(configService),
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
