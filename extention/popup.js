e084b52a3aac34b430dceffb40a990635c27067e
jerry <kcjerim1234567@gmail.com>
2024-12-26 22:04:24 +0900

    feat: 크롬 extenstion 시작
    폴더 생성

---
+++ extention/popup.js
@@ -0,0 +1,7 @@
+document.getElementById('summarize').addEventListener('click', () => {
+    alert('영상 요약 기능 준비 중입니다!');
+});
+
+document.getElementById('memo').addEventListener('click', () => {
+    alert('메모 기능 준비 중입니다!');
+});
