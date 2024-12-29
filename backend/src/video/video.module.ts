0dfd06b5dc868222c424d8a8f6e820356811069e
Choi JinYoung <chio6632@gmail.com>
2024-12-29 15:46:54 +0900

    feat: channel 모듈을 video 모듈에서 사용하기 때문에 exports와 imports에 추가

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelModule } from 'src/channel/channel.module';
 import { YoutubeauthModule } from 'src/youtubeauth/youtubeauth.module';
 import { VideoController } from './video.controller';
 import { Video } from './video.entity';
 import { VideoService } from './video.service';
-
 @Module({
-    imports: [TypeOrmModule.forFeature([Video]), YoutubeauthModule],
+    imports: [TypeOrmModule.forFeature([Video]), YoutubeauthModule, ChannelModule],
     providers: [VideoService],
     controllers: [VideoController],
 })
