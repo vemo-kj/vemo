a969f17d5479c75e4c71e407874bede3af2bfdfc
Choi JinYoung <chio6632@gmail.com>
2024-12-30 21:15:25 +0900

    feat: jwttoken expiry time 삭제

--- backend/src/auth/jwt/jwt.strategy.ts
+++ backend/src/auth/jwt/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: '1h',
+            ignoreExpiration: false,
             secretOrKey: process.env.JWT_SECRET,
         });
     }
