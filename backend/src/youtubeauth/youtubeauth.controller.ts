62f7687f45a527f348f9e60bce71c6ded04c04bb
Choi JinYoung <chio6632@gmail.com>
2024-12-30 21:11:09 +0900

    feat: youtube OAuth 인증로직 작성 및 확인, session에 저장하는 코드 삭제

--- backend/src/youtubeauth/youtubeauth.controller.ts
+++ backend/src/youtubeauth/youtubeauth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Query, Redirect, Session } from '@nestjs/common';
+import { Controller, Get, Query, Redirect } from '@nestjs/common';
 import { YoutubeauthService } from './youtubeauth.service';
 
 @Controller('youtubeauth')
 export class YoutubeauthController {
     constructor(private readonly youtubeauthService: YoutubeauthService) {}
-    @Get('auth')
+
+    @Get()
     @Redirect()
     redirectToAuth() {
         return { url: this.youtubeauthService.getAuthUrl() };
@@ -12,13 +13,8 @@ export class YoutubeauthController {
 
     @Get('OAuth2Callback')
     @Redirect()
-    async oauth2Callback(@Query('code') code: string, @Session() session: Record<string, any>) {
-        const tokens = await this.youtubeauthService.getAccessToken(code);
-        await this.youtubeauthService.setTokenForSession(session.id, tokens);
-
-        const returnUrl = session.returnTo || '/';
-        delete session.returnTo;
-
-        return { url: returnUrl };
+    async oauth2Callback(@Query('code') code: string) {
+        const redirectUrl = await this.youtubeauthService.handleOAuthCallback(code);
+        return { url: redirectUrl };
     }
 }
