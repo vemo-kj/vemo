1f37e5281469f291de4ee9e416460794cc491b29
jerry <kcjerim1234567@gmail.com>
2024-12-30 17:32:43 +0900

    fix: channel modules 부분 exports 추가

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -4,9 +4,10 @@ import { YoutubeauthModule } from 'src/youtubeauth/youtubeauth.module';
 import { VideoController } from './video.controller';
 import { Video } from './video.entity';
 import { VideoService } from './video.service';
+import { ChannelModule } from 'src/channel/channel.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Video]), YoutubeauthModule],
+    imports: [TypeOrmModule.forFeature([Video]), YoutubeauthModule, ChannelModule],
     providers: [VideoService],
     controllers: [VideoController],
 })
