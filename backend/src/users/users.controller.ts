4882670048573d47cdaa3b3da08609049d090d2c
Choi JinYoung <chio6632@gmail.com>
2024-12-30 15:44:10 +0900

    feat: 회원가입, 로그인, 로그아웃 기능 구현

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { LoginRequestDto } from 'src/auth/dto/login.requests.dto';
+import { Public } from 'src/public.decorator';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+import { SignupRequestsDto } from './dto/signup.requests.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly authService: AuthService,
+    ) {}
+
+    @Public()
+    @Post('signup')
+    async signUp(@Body() dto: SignupRequestsDto) {
+        return this.usersService.signUp(dto);
+    }
+
+    @Public()
+    @Post('login')
+    async login(@Body() dto: LoginRequestDto) {
+        return this.authService.signIn(dto);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Post('logout')
+    async logout(@Request() req) {
+        return this.authService.signOut(req.user.sub);
+    }
+}
