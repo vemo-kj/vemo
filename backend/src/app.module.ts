130cc40f7565e06fefd0786eed1aba79afe1b449
Choi JinYoung <chio6632@gmail.com>
2024-12-30 15:45:52 +0900

    feat: jwt 인증 로직 작성 및 글로벌로 적용

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/jwt/jwt.guard';
 import { ChannelModule } from './channel/channel.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { MemosModule } from './memos/memos.module';
+import { UsersModule } from './users/users.module';
 import { VideoModule } from './video/video.module';
 import { YoutubeauthModule } from './youtubeauth/youtubeauth.module';
 
@@ -24,8 +28,16 @@ import { YoutubeauthModule } from './youtubeauth/youtubeauth.module';
         VideoModule,
         ChannelModule,
         YoutubeauthModule,
+        UsersModule,
+        AuthModule,
     ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: JwtAuthGuard,
+        },
+    ],
 })
 export class AppModule {}
