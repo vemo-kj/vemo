6e4d10beccb813b5d6598c5e7e113f664b825780
Choi JinYoung <chio6632@gmail.com>
2024-12-30 21:17:01 +0900

    feat: config 모듈 추가하는 부분 작성

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -12,10 +13,13 @@ import { JwtStrategy } from './jwt/jwt.strategy';
     imports: [
         TypeOrmModule.forFeature([User]),
         PassportModule.register({ defaultStrategy: 'jwt', session: true }),
-        JwtModule.register({
-            global: true,
-            secret: process.env.JWT_SECRET,
-            signOptions: { expiresIn: '1h' },
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: { expiresIn: '1h' },
+            }),
+            inject: [ConfigService],
         }),
         YoutubeauthModule,
         forwardRef(() => UsersModule),
