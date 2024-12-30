6ee98a03932a28edfbb1fe9216b8ae0f3536a44f
jerry <kcjerim1234567@gmail.com>
2024-12-30 13:39:19 +0900

    subtitles 모듈 시작

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,4 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SummaryModule } from './summary/summary.module';
-import { SubtitlesModule } from './subtitles/subtitles.module';
-import { SummarizationModule } from './summarization/summarization.module';
-import { QuizModule } from './quiz/quiz.module';
-=======
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
@@ -17,19 +8,11 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { MemosModule } from './memos/memos.module';
 import { VideoModule } from './video/video.module';
 import { YoutubeauthModule } from './youtubeauth/youtubeauth.module';
->>>>>>> a4e9f4cc03ba9c608d8065ea42c5aecfd11eaf9d
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-<<<<<<< HEAD
-        }),
-        SummaryModule,
-        SubtitlesModule,
-        SummarizationModule,
-        QuizModule,
-=======
             envFilePath: '.env',
         }),
         TypeOrmModule.forRootAsync({
@@ -42,7 +25,6 @@ import { YoutubeauthModule } from './youtubeauth/youtubeauth.module';
         VideoModule,
         ChannelModule,
         YoutubeauthModule,
->>>>>>> a4e9f4cc03ba9c608d8065ea42c5aecfd11eaf9d
     ],
     controllers: [AppController],
     providers: [AppService],
