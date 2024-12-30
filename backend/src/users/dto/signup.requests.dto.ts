a0b97c172eafb5c416676fae5e779bb687779638
Choi JinYoung <chio6632@gmail.com>
2024-12-30 15:43:11 +0900

    feat: signuprequestsdto 작성

---
+++ backend/src/users/dto/signup.requests.dto.ts
@@ -0,0 +1,36 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
+
+export class SignupRequestsDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @Length(8, 60)
+    @IsNotEmpty()
+    password: string;
+
+    @IsDate()
+    @IsNotEmpty()
+    @Type(() => Date)
+    birth: Date;
+
+    @IsEnum(['Male', 'Female', 'Other'])
+    @IsNotEmpty()
+    gender: string;
+
+    @IsString()
+    @Length(1, 30)
+    @IsNotEmpty()
+    nickname: string;
+
+    @IsOptional()
+    @IsString()
+    profileImage?: string;
+
+    @IsOptional()
+    @IsString()
+    @Length(0, 255)
+    introduction?: string;
+}
