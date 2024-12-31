14ef53b58629f616870c2acc10ad1e6801408fb9
jerry <kcjerim1234567@gmail.com>
2024-12-31 13:48:41 +0900

    feat: quiz 모듈 시작

--- backend/src/quiz/quiz.module.ts
+++ backend/src/quiz/quiz.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
+import { QuizController } from './quiz.controller';
+import { SubtitlesModule } from 'src/subtitles/subtitles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [QuizService]
+    imports: [
+        // TypeOrmModule.forFeature([Summaries, Summary]), // 엔티티 등록
+        ConfigModule,
+        SubtitlesModule,
+    ],
+    providers: [QuizService],
+    controllers: [QuizController],
 })
 export class QuizModule {}
--- backend/src/quiz/quiz.service.ts
+++ backend/src/quiz/quiz.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class QuizService {}
+export class QuizService {
+    constructor(
+        private readonly subtitlesService: SubtitlesService,
+        private readonly summarizationService: SummarizationService,
+    ) {}
+
+    @Get()
+    async summarizeVideo(@Query('videoid') videoid: string) {
+        const url = `https://www.youtube.com/watch?v=${videoid}`;
+        const subtitles = await this.subtitlesService.getVideoSubtitles(url);
+        return this.summarizationService.extractSummary(subtitles, videoid);
+    }
+}
