8bffa4541b674426a7c9edb8ac0e39caebc2957f
Choi JinYoung <chio6632@gmail.com>
2025-01-01 19:42:25 +0900

    feat: home module merge

--- backend/src/home/home.module.ts
+++ backend/src/home/home.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { HomeService } from './home.service';
-import { HomeController } from './home.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Video } from '../video/video.entity';
-import { Channel } from '../channel/channel.entity';
-import { VideoModule } from 'src/video/video.module';
-import { MemosService } from 'src/memos/memos.service';
 import { MemosModule } from 'src/memos/memos.module';
-import { VideoService } from 'src/video/video.service';
-import { PlaylistService } from 'src/playlist/playlist.service';
 import { PlaylistModule } from 'src/playlist/playlist.module';
+import { VideoModule } from 'src/video/video.module';
+import { Channel } from '../channel/channel.entity';
+import { Video } from '../video/video.entity';
+import { HomeController } from './home.controller';
+import { HomeService } from './home.service';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Video, Channel]), VideoModule, MemosModule, PlaylistModule],
