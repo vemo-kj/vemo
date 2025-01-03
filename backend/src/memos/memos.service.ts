d82b2423fc57de19e43938c5ae5c88d521db7419
서성진 <tjtjdwls0614@gmail.com>
2025-01-03 23:53:51 +0900

    feat: 커뮤니티 api 설계 완료, 테스트 완

--- backend/src/memos/memos.service.ts
+++ backend/src/memos/memos.service.ts
@@ -12,7 +12,7 @@ export class MemosService {
         @InjectRepository(Memos) private readonly memosRepository: Repository<Memos>,
         @InjectRepository(Users) private readonly userRepository: Repository<Users>,
         @InjectRepository(Video) private readonly videoRepository: Repository<Video>,
-    ) {}
+    ) { }
 
     async createMemos(
         memosTitle: string,
@@ -60,7 +60,7 @@ export class MemosService {
     async getAllMemosByVideo(videoId: string): Promise<Memos[]> {
         return await this.memosRepository.find({
             where: { video: { id: videoId } },
-            relations: ['user', 'video', 'memos'],
+            relations: ['user', 'video'],
         });
     }
 
