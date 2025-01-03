c5c3f4f4fc672b45fc21077bbac44867813e028c
jerry <kcjerim1234567@gmail.com>
2025-01-03 23:40:21 +0900

    test: please

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'VEMO HELLO KRAFTON!';
+        return 'HELLO Jerry !!';
     }
 }
