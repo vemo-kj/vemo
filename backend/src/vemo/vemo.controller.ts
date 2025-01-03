9e7271712cf912962c17049b778c2a27d74db449
SoohoPark <clapp301@gmail.com>
2025-01-03 20:37:27 +0900

    feat: vemoController 요청 url 변경 및 createUserPlaylist 삭제

--- backend/src/vemo/vemo.controller.ts
+++ backend/src/vemo/vemo.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, Query, Req } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
 import { VemoService } from './vemo.service';
 import { GetCommunityMemosResponseDto } from './dto/get-community-memos-response.dto';
 import { PlaylistResponseDto } from '../playlist/dto/playlist-response.dto';
-import { CreatePlaylistDto } from '../playlist/dto/create-playlist.dto';
 import { RequestWithUserInterface } from '../auth/interface/request-with-user.interface';
 
 @Controller('vemo')
@@ -12,7 +11,8 @@ export class VemoController {
     /**
      * 커뮤니티 메모를 조회
      * @param videoId 비디오 ID
-     * @param getCommunityMemosDto 조회 옵션 DTO
+     * @param filter
+     * @param req
      * @returns GetCommunityMemosResponseDto
      */
     @Get('video/:videoId/community')
@@ -27,27 +27,11 @@ export class VemoController {
 
     /**
      * 사용자 재생목록 조회
-     * @param userId 사용자 ID
+     * @param req
      * @returns 사용자 재생목록 목록
      */
-    @Get('playlists/:userId')
-    async getUserPlaylists(
-        @Param('userId', ParseIntPipe) userId: number,
-    ): Promise<PlaylistResponseDto[]> {
-        return await this.vemoService.getUserPlaylists(userId);
-    }
-
-    /**
-     * 사용자 재생목록 생성
-     * @param createPlaylistDto 생성 DTO
-     * @param req
-     * @returns 생성된 재생목록 정보
-     */
-    @Post('playlist')
-    async createUserPlaylist(
-        @Body() createPlaylistDto: CreatePlaylistDto,
-        @Req() req: RequestWithUserInterface,
-    ): Promise<PlaylistResponseDto> {
-        return await this.vemoService.createUserPlaylist(createPlaylistDto, req.user.id);
+    @Get('playlists/:playlistId')
+    async getUserPlaylists(@Req() req: RequestWithUserInterface): Promise<PlaylistResponseDto[]> {
+        return await this.vemoService.getUserPlaylists(req.user.id);
     }
 }
