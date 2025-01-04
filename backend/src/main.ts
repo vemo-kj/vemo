b960f22c9167c219d93b8c1ca811830e3f9107ff
서성진 <tjtjdwls0614@gmail.com>
2025-01-04 16:19:02 +0900

    feat: 플레이리스트 기능 구현 및 api통신 모두 완료

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
         DB_HOST: process.env.DB_HOST,
     });
     app.enableCors({
-        origin: '*',
+        origin: 'http://localhost:3000',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
