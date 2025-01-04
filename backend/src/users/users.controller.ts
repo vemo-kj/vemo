de2fe4c237e2a6b7037085cbab8c5208a552dd60
Choi JinYoung <chio6632@gmail.com>
2025-01-04 16:14:47 +0900

    feat: user profile 정보 업데이트 컨트롤러, 비즈니스 로직 작성

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import { Body, Controller, Post, Put, Request } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { LoginRequestDto } from 'src/auth/dto/login.requests.dto';
 import { Public } from '../public.decorator';
 import { SignupRequestsDto } from './dto/signup.requests.dto';
+import { UpdateUserDto } from './dto/updateUser.requests.dto';
 import { UsersService } from './users.service';
-
 @Controller('users')
 export class UsersController {
     constructor(
@@ -27,5 +27,10 @@ export class UsersController {
     @Post('logout')
     async logout(@Request() req) {
         return this.authService.signOut(req.user.sub);
+    }
+
+    @Put('update')
+    async update(@Request() req, @Body() dto: UpdateUserDto) {
+        return this.usersService.updateUser(req.user.sub, dto);
     }
 }
