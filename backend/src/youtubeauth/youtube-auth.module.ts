5b41ee1b05b6cf60f7e2ab13afe9d9aa467d0152
Choi JinYoung <chio6632@gmail.com>
2025-01-05 19:20:26 +0900

    remove: youtubeauth controller 삭제

--- backend/src/youtubeauth/youtube-auth.module.ts
+++ backend/src/youtubeauth/youtube-auth.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { YoutubeAuthController } from './youtube-auth.controller';
 import { YoutubeAuthService } from './youtube-auth.service';
 
 @Module({
     imports: [ConfigModule],
     providers: [YoutubeAuthService],
     exports: [YoutubeAuthService],
-    controllers: [YoutubeAuthController],
 })
 export class YoutubeAuthModule {}
