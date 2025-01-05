a058cf05ab1b061d33d6d8dc65416eca76ee8bb4
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-01-05 20:29:23 +0900

    feat: pdf service 구현 및 axios & 폰트 다운로드
    - pdf 추출을 위해 폰트 다운

--- backend/src/pdf/pdf.controller.ts
+++ backend/src/pdf/pdf.controller.ts
@@ -15,11 +15,13 @@ export class PdfController {
     @Get('download/:memosId')
     async downloadPDF(@Param('memosId') memosId: number, @Res() res: Response) {
         // 실제 데이터 조회
+
         const memos = await this.memosService.getMemosById(memosId);
-        const { memo, capture } = memos;
+        const { title, memo, capture } = memos;
 
-        const pdfBuffer = await this.pdfService.createMemoCapturePDF(memo, capture);
+        const pdfBuffer = await this.pdfService.createMemoCapturePDF(title, memo, capture);
 
+        console.log(pdfBuffer);
         res.set({
             'Content-Type': 'application/pdf',
             'Content-Disposition': 'attachment; filename=memo_timeline.pdf',
