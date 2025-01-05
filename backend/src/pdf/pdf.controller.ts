dce89571e0b1ed1c13602bcfe9ee32b3dc739f98
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-01-05 17:08:49 +0900

    chore: 로그 찍어보기

--- backend/src/pdf/pdf.controller.ts
+++ backend/src/pdf/pdf.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { MemosService } from 'src/memos/memos.service';
+import { PdfService } from 'src/pdf/pdf.service';
+import { Public } from 'src/public.decorator';
 
 @Controller('pdf')
-export class PdfController {}
+export class PdfController {
+    constructor(
+        private readonly memosService: MemosService,
+        private readonly pdfService: PdfService,
+    ) {}
+
+    @Public()
+    @Get(':memosId')
+    async downloadPdf(@Param('memosId', ParseIntPipe) memosId: number) {
+        console.log('✨Hello');
+        const memos = await this.memosService.getMemosById(memosId);
+        console.log(memos);
+        // PDF 생성 로직 추가
+    }
+}
--- backend/src/pdf/pdf.module.ts
+++ backend/src/pdf/pdf.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PdfController } from './pdf.controller';
 import { PdfService } from './pdf.service';
+import { MemosModule } from 'src/memos/memos.module';
 
 @Module({
-  controllers: [PdfController],
-  providers: [PdfService]
+    imports: [MemosModule],
+    controllers: [PdfController],
+    providers: [PdfService],
 })
 export class PdfModule {}
