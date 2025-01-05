c670ee1cbaa871dc528fc443756cd5380a5f5972
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-01-05 19:59:51 +0900

    feat: controller 엔드포인트 작성
    - pdf/download/memoseId 작성
    - 불필요한 spec 파일 삭제

--- backend/src/pdf/pdf.controller.ts
+++ backend/src/pdf/pdf.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Res } from '@nestjs/common';
 import { MemosService } from 'src/memos/memos.service';
+import { Response } from 'express'; // 추가
 import { PdfService } from 'src/pdf/pdf.service';
 import { Public } from 'src/public.decorator';
 
@@ -11,11 +12,19 @@ export class PdfController {
     ) {}
 
     @Public()
-    @Get(':memosId')
-    async downloadPdf(@Param('memosId', ParseIntPipe) memosId: number) {
-        console.log('✨Hello');
+    @Get('download/:memosId')
+    async downloadPDF(@Param('memosId') memosId: number, @Res() res: Response) {
+        // 실제 데이터 조회
         const memos = await this.memosService.getMemosById(memosId);
-        console.log(memos);
-        // PDF 생성 로직 추가
+        const { memo, capture } = memos;
+
+        const pdfBuffer = await this.pdfService.createMemoCapturePDF(memo, capture);
+
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename=memo_timeline.pdf',
+        });
+
+        res.send(pdfBuffer);
     }
 }
