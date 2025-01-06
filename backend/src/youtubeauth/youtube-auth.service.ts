ae8c49096b4451d0a2175453b4a0a44751dfe39e
Choi JinYoung <chio6632@gmail.com>
2025-01-06 15:33:04 +0900

    fix: exports service 이름 오류 고치기

--- backend/src/youtubeauth/youtube-auth.service.ts
+++ backend/src/youtubeauth/youtube-auth.service.ts
@@ -25,7 +25,6 @@ export class YoutubeAuthService implements OnModuleInit {
 
         this.oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 
-        // 초기 인증 설정
         this.initializeYoutubeClient(refreshToken);
     }
 
