d9f6c60280c1cffbd9c890e1a959fa12d2b80154
SoohoPark <clapp301@gmail.com>
2025-01-07 23:40:00 +0900

    chore: redis 의존성 수정

--- backend/src/config/redis.config.ts
+++ backend/src/config/redis.config.ts
@@ -1,21 +1,17 @@
-// import { CacheModuleOptions, CacheOptionsFactory } from '@nestjs/cache-manager';
-// import { Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-// import { redisStore } from 'cache-manager-redis-yet';
+import { CacheModuleOptions } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-ioredis';
 
 // @Injectable()
 // export class RedisConfigService implements CacheOptionsFactory {
 //     constructor(private configService: ConfigService) {}
 
-//     createCacheOptions(): CacheModuleOptions {
-//         return {
-//             store: redisStore,
-//             socket: {
-//                 host: this.configService.get<string>('REDIS_HOST', 'localhost'),
-//                 port: this.configService.get<number>('REDIS_PORT', 6379),
-//                 password: this.configService.get<string>('REDIS_PASSWORD'),
-//             },
-//             ttl: 60 * 60 * 24,
-//         };
-//     }
-// }
+    createCacheOptions(): CacheModuleOptions {
+        return {
+            store: redisStore,
+            host: this.configService.get<string>('REDIS_HOST', 'localhost'),
+            port: this.configService.get<number>('REDIS_PORT', 6379),
+            password: this.configService.get<string>('REDIS_PASSWORD'),
+            ttl: 60 * 60 * 24,
+        };
+    }
+}
