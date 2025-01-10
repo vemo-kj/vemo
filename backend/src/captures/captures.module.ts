3f3735535b2a1a98da50faf514d7fd95f695bb09
Kimjerim <95560713+jerry-1211@users.noreply.github.com>
2025-01-10 14:14:08 +0900

    s3 의존성 주입

--- backend/src/captures/captures.module.ts
+++ backend/src/captures/captures.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CapturesController } from './captures.controller';
 import { Captures } from './captures.entity';
 import { CapturesService } from './captures.service';
+import { AppModule } from 'src/app.module';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Captures])],
+    imports: [TypeOrmModule.forFeature([Captures]), S3Module],
     providers: [CapturesService],
     controllers: [CapturesController],
 })
