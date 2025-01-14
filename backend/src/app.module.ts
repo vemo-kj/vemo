ce580689e1edbfbbb88a955b7419f6618da45be2
Choi JinYoung <chio6632@gmail.com>
2025-01-14 16:13:03 +0900

    feat: cache 모듈 import 방식 변경

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,30 +2,30 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as redisStore from 'cache-manager-ioredis';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt/jwt.guard';
+import { CapturesModule } from './captures/captures.module';
 import { ChannelModule } from './channel/channel.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { HomeModule } from './home/home.module';
 import { MemosModule } from './memos/memos.module';
+import { PlaylistModule } from './playlist/playlist.module';
+import { QuizModule } from './quiz/quiz.module';
+import { S3Module } from './s3/s3.module';
 import { SubtitlesModule } from './subtitles/subtitles.module';
+import { SummarizationModule } from './summarization/summarization.module';
+import { TextExtractionModule } from './text-extraction/text-extraction.module';
 import { UsersModule } from './users/users.module';
+import { VemoModule } from './vemo/vemo.module';
 import { VideoModule } from './video/video.module';
 import { YoutubeAuthModule } from './youtubeauth/youtube-auth.module';
-import { CapturesModule } from './captures/captures.module';
-import { QuizModule } from './quiz/quiz.module';
-import { SummarizationModule } from './summarization/summarization.module';
-import { VemoModule } from './vemo/vemo.module';
-import { HomeModule } from './home/home.module';
-import { PlaylistModule } from './playlist/playlist.module';
-import { TextExtractionModule } from './text-extraction/text-extraction.module';
-import { S3Module } from './s3/s3.module';
-
 // import { RedisConfigService } from './config/redis.config';
-// import { CacheModule } from '@nestjs/cache-manager';
-import { PdfModule } from './pdf/pdf.module';
+import { CacheModule } from '@nestjs/cache-manager';
 import { MemoModule } from './memo/memo.module';
+import { PdfModule } from './pdf/pdf.module';
 
 @Module({
     imports: [
@@ -38,11 +38,13 @@ import { MemoModule } from './memo/memo.module';
             useFactory: (configService: ConfigService) => typeOrmConfig(configService),
             inject: [ConfigService],
         }),
-        // CacheModule.registerAsync({
-        //     imports: [ConfigModule],
-        //     useClass: RedisConfigService,
-        //     isGlobal: true,
-        // }),
+        CacheModule.registerAsync({
+            useFactory: () => ({
+                store: redisStore as unknown as string,
+                host: process.env.REDIS_HOST,
+                port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+            }),
+        }),
         MemoModule,
         MemosModule,
         VideoModule,
